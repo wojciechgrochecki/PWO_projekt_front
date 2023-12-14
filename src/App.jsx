6a36a74d487043e88a6c716631b52b31e9119fd0
Wojciech Grochecki <wojciech5522@gmail.com>
2023-12-14 10:54:45 +0100

    build(project): migrate to vite

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import RegistrationForm from "./components/Register/RegistrationForm";
+import LoginForm from "./components/Login/LoginForm";
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./hooks/ProtectedRoute/ProtectedRoute";
+import { UserProvider } from "./hooks/auth";
+import MainPage from "./components/MainPage/MainPage";
+import AdminPanel from "./components/AdminPanel/AdminPanel/AdminPanel";
+import { ToastContainer } from "react-toastify";
+
+function App() {
+  return (
+    <>
+      <UserProvider>
+        <Routes>
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/adminPanel" element={<AdminPanel />} />
+          </Route>
+        </Routes>
+      </UserProvider>
+      <ToastContainer hideProgressBar autoClose={1500} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
+
+axios.defaults.baseURL = "http://localhost:8080/api/v1";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
