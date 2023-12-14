049f0ac7f536b10380926ab821848aaa2423a7bd
Wojciech Grochecki <wojciech5522@gmail.com>
2023-12-14 10:21:17 +0100

    feat(auth): add auth hook to manage user token

---
+++ src/hooks/auth/index.js
@@ -0,0 +1,50 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const tokenFromStorage = localStorage.getItem("token");
+  const [token, setToken] = useState(tokenFromStorage);
+
+  if (token) {
+    const decodedToken = jwt_decode(token);
+    const currentTime = Date.now() / 1000;
+
+    if (decodedToken.exp < currentTime) {
+      setToken(null);
+    }
+  }
+
+  const login = async (formData) => {
+    const url = "http://localhost:8080/api/Auth/Login";
+    console.log(formData);
+    const { data: res } = await axios.post(url, formData);
+    const newToken = res.data;
+    localStorage.setItem("token", newToken);
+    setToken(newToken);
+
+    navigate("/");
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    navigate("/login");
+  };
+
+  const value = {
+    token,
+    login,
+    logout,
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+export const useAuth = () => {
+  return useContext(UserContext);
+};
