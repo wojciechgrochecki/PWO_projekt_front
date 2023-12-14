8a5f30aceab58b89ffbc2c2fa7698257ad25b7aa
Wojciech Grochecki <wojciech5522@gmail.com>
2023-12-14 11:43:00 +0100

    feat(chart): accept data and title in single parameter

--- src/components/MainPage/LineChart/index.jsx
+++ src/components/MainPage/LineChart/index.jsx
@@ -1,31 +1,29 @@
-
 import React from "react";
 import { Line } from "react-chartjs-2";
-import style from './styles.module.css'
+import style from "./styles.module.css";
 
-
-
-function LineChart({ chartData, title }) {
-    return (
-        <div className={style["chart-container"]}>
-            <Line
-                data={chartData}
-                options={{
-                    plugins: {
-                        title: {
-                            display: true,
-                            text: title,
-                            font: {
-                                size: 30
-                            }
-                        },
-                        legend: {
-                            display: true
-                        }
-                    }
-                }}
-            />
-        </div>
-    );
+function LineChart({ chartData }) {
+  const { title, ...rest } = chartData;
+  return (
+    <div className={style["chart-container"]}>
+      <Line
+        data={rest}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: title,
+              font: {
+                size: 30,
+              },
+            },
+            legend: {
+              display: true,
+            },
+          },
+        }}
+      />
+    </div>
+  );
 }
 export default LineChart;
