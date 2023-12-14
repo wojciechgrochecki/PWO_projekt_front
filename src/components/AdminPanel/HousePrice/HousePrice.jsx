659c396a1746e86a5e7fd4be0b9271b3025373e8
Wojciech Grochecki <wojciech5522@gmail.com>
2023-12-14 11:53:12 +0100

    feat(house-price): add basic CRUD for house price management

--- src/components/AdminPanel/HousePrice/HousePrice.jsx
+++ src/components/AdminPanel/HousePrice/HousePrice.jsx
@@ -1,275 +1,283 @@
-import style from './styles.module.css'
-import axios from 'axios'
-import { useAuth } from '../../../hooks/auth'
-import toast, { Toaster } from 'react-hot-toast';
-import { useState } from 'react'
+import style from "./styles.module.css";
+import axios from "axios";
+import { useAuth } from "../../../hooks/auth";
+import { useEffect, useState } from "react";
 
 export default function HousePrice() {
-    const [selectedComponent, setSelectedComponent] = useState(0);
+  const { token } = useAuth();
+  const [countries, setCountries] = useState(null);
+  const [selectedCountryHousePrices, setSelectedCountryHousePrices] =
+    useState(null);
+  const [housePrice, setHousePrice] = useState(null);
 
-    return (<div className={style["two-row-grid"]}>
-        <div className={style['button-panel']}>
-            <button className={style['button-get']} onClick={() => setSelectedComponent(0)} >Get</button>
-            <button className={style['button-post']} onClick={() => setSelectedComponent(1)} >Post</button>
-            <button className={style['button-put']} onClick={() => setSelectedComponent(2)}>Put</button>
-            <button className={style['button-delete']} onClick={() => setSelectedComponent(3)}  >Delete</button>
-        </div>
-        <div className={style['form-panel']}>
-            {selectedComponent == 0 && <GetForm />}
-            {selectedComponent == 1 && <PostForm />}
-            {selectedComponent == 2 && <PutForm />}
-            {selectedComponent == 3 && <DeleteForm />}
-        </div>
-    </div>)
-}
-
-function GetForm() {
-    const [selectedId, setSelectedId] = useState(1);
-    const [textAreaContent, setTextAreaContent] = useState('');
-    const [gotData, setGotData] = useState(0);
-    const { token } = useAuth();
-    console.log(selectedId);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        let url = `http://localhost:8080/api/HousePrice/${selectedId}`;
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        };
-        axios.get(url, config)
-            .then(response => {
-                setGotData(1);
-                setTextAreaContent(JSON.stringify(response.data.data, null, 2));
-                console.log(response.data);
-            })
-            .catch(error => {
-                setGotData(1);
-                setTextAreaContent(JSON.stringify(error.response.data, null, 2));
-            });
-
-    }
-
-
-    return (
-        <div className={style["form-wrap"]}>
-            <form className={style["get-form"]} onSubmit={e => handleSubmit(e)}>
-                <div>Select HousePrice Id:</div>
-                <input type="number" min="1" step="1" value={selectedId}
-                    onChange={e => setSelectedId(e.target.value)} />
-                <button>Submit</button>
-            </form>
-            {gotData != 0 &&
-                <textarea rows={12}
-                    value={textAreaContent}
-                    onChange={e => setTextAreaContent(e.target.value)}
-                />
-            }
-        </div>
-    )
-}
-
-function PostForm() {
-    const [formData, setFormData] = useState(
-        {
-            year: "2020", value: "110", countryId: "1",
-            measureId: "2"
+  useEffect(() => {
+    if (countries == null) {
+      axios
+        .get("countries", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         })
-    const [textAreaContent, setTextAreaContent] = useState('');
-    const [gotData, setGotData] = useState(0);
-    const { token } = useAuth();
-
-    function handleChange(e) {
-        const { name, value } = e.target;
-        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }))
+        .then((response) => {
+          setCountries(response.data);
+        });
     }
+  });
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        let url = `http://localhost:8080/api/HousePrice`;
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        };
-        axios.post(url, formData, config)
-            .then(response => {
-                setGotData(1);
-                setTextAreaContent(JSON.stringify(response.data.data, null, 2));
-                console.log(response.data);
-            })
-            .catch(error => {
-                setGotData(1);
-                setTextAreaContent(JSON.stringify(error.response.data, null, 2));
-            });
+  async function fetchCountryInfo(e) {
+    axios
+      .get(`countries/${e.target.value}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        let data = response.data;
+        const filteredData = data.housePrices.filter(
+          (house) => house.housePriceMeasure.subject === "REAL"
+        );
+        filteredData["countryId"] = e.target.value;
+        setSelectedCountryHousePrices(filteredData);
+      });
+  }
 
-    }
+  function handleYearSelection(e) {
+    setHousePrice(selectedCountryHousePrices[e.target.value]);
+  }
 
-
-    return (
-        <div className={style["form-wrap"]}>
-            <form onSubmit={handleSubmit}>
-                <div className={style["input-wrap"]}>
-                    <div>Year:</div>
-                    <input type="number" name="year" min="1" step="1" value={formData.year}
-                        onChange={handleChange} />
-                </div>
-                <div className={style["input-wrap"]}>
-                    <div>Value:</div>
-                    <input type="number" name="value" value={formData.value}
-                        onChange={handleChange} />
-                </div>
-                <div className={style["input-wrap"]}>
-                    <div>Measure Id:</div>
-                    <input type="number" name="measureId" min="1" step="1" max="2" value={formData.measureId}
-                        onChange={handleChange} />
-                </div>
-                <div className={style["input-wrap"]}>
-                    <div>Country Id:</div>
-                    <input type="number" name="countryId" min="1" step="1" max="14" value={formData.countryId}
-                        onChange={handleChange} />
-                </div>
-                <button>Submit</button>
-            </form>
-
-
-            {gotData != 0 &&
-                <textarea rows={12}
-                    value={textAreaContent}
-                    onChange={e => setTextAreaContent(e.target.value)}
-                />
-            }
-        </div>)
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+      {countries ? (
+        <label>
+          Wybierz kraj:
+          <select name="country" id="country" onInput={fetchCountryInfo}>
+            {countries.map((country) => (
+              <option key={country.id} value={country.id}>
+                {country.countryName}
+              </option>
+            ))}
+          </select>
+        </label>
+      ) : (
+        <p>Nie ma danych w bazie</p>
+      )}
+      {selectedCountryHousePrices && (
+        <label>
+          Wybierz rok:
+          <select
+            className={style.select}
+            name="year"
+            id="year"
+            onInput={handleYearSelection}
+          >
+            {selectedCountryHousePrices.map((house, index) => (
+              <option key={house.id} value={index}>
+                {house.year}
+              </option>
+            ))}
+          </select>
+        </label>
+      )}
+      {housePrice !== null && (
+        <>
+          <DisplayKeyValuePairs data={housePrice} />
+          <div className={style.forms_container}>
+            <PostForm
+              house={housePrice}
+              countryId={selectedCountryHousePrices.countryId}
+            />
+            <PutForm
+              house={housePrice}
+              countryId={selectedCountryHousePrices.countryId}
+            />
+            <DeleteForm house={housePrice} />
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
+const DisplayKeyValuePairs = ({ data }) => {
+  const { id, value, year } = data;
 
+  return (
+    <div>
+      <ul className={style.pairs_list}>
+        <li>id:{id}</li>
+        <li>wartość:{value}</li>
+        <li>rok:{year}</li>
+      </ul>
+    </div>
+  );
+};
 
-function DeleteForm() {
-    const [selectedId, setSelectedId] = useState(94);
-    const [textAreaContent, setTextAreaContent] = useState('');
-    const [gotData, setGotData] = useState(0);
-    const { token } = useAuth();
-    console.log(selectedId);
+function PostForm({ house, countryId }) {
+  const [formData, setFormData] = useState({
+    year: house.year,
+    value: house.value,
+    countryId: countryId,
+    measureId: "2",
+  });
+  const { token } = useAuth();
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        let url = `http://localhost:8080/api/HousePrice/${selectedId}`;
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        };
-        axios.delete(url, config)
-            .then(response => {
-                setGotData(1);
-                setTextAreaContent(JSON.stringify(response.data.data, null, 2));
-                console.log(response.data);
-            })
-            .catch(error => {
-                setGotData(1);
-                setTextAreaContent(JSON.stringify(error.response.data, null, 2));
-            });
+  useEffect(() => {
+    setFormData({
+      year: house.year,
+      value: house.value,
+      countryId: countryId,
+      measureId: "2",
+    });
+  }, [house, countryId]);
 
-    }
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  }
 
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    return (
-        <div className={style["form-wrap"]}>
-            <form onSubmit={e => handleSubmit(e)}>
-                <div className={style["input-wrap"]}>
-                    <div>Select HousePrice Id to delete:</div>
-                    <input type="number" min="1" step="1" value={selectedId}
-                        onChange={e => setSelectedId(e.target.value)} />
-                </div>
-                <button>Submit</button>
-            </form>
-            {gotData != 0 &&
-                <textarea rows={6}
-                    value={textAreaContent}
-                    onChange={e => setTextAreaContent(e.target.value)}
-                />
-            }
+    let url = `housePrices`;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios
+      .post(url, formData, config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {});
+  }
+
+  return (
+    <div className={style["form-wrap"]}>
+      <p>POST</p>
+      <form onSubmit={handleSubmit}>
+        <div className={style["input-wrap"]}>
+          <div>Rok:</div>
+          <input
+            type="number"
+            name="year"
+            min="1"
+            step="1"
+            value={formData.year}
+            onChange={handleChange}
+          />
         </div>
-    )
+        <div className={style["input-wrap"]}>
+          <div>Wartość:</div>
+          <input
+            type="number"
+            name="value"
+            value={formData.value}
+            onChange={handleChange}
+          />
+        </div>
+        <button>Wyślij</button>
+      </form>
+    </div>
+  );
 }
 
+function DeleteForm({ house }) {
+  const { token } = useAuth();
 
-function PutForm() {
-    const [formData, setFormData] = useState(
-        {
-            id: "48", year: "1970", value: "100", countryId: "1",
-            measureId: "2"
-        })
-    const [textAreaContent, setTextAreaContent] = useState('');
-    const [gotData, setGotData] = useState(0);
-    const { token } = useAuth();
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleChange(e) {
-        const { name, value } = e.target;
-        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }))
-    }
+    let url = `housePrices/${house.id}`;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios
+      .delete(url, config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {});
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        let url = `http://localhost:8080/api/HousePrice`;
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        };
-        axios.put(url, formData, config)
-            .then(response => {
-                setGotData(1);
-                setTextAreaContent(JSON.stringify(response.data.data, null, 2));
-                console.log(response.data);
-            })
-            .catch(error => {
-                setGotData(1);
-                setTextAreaContent(JSON.stringify(error.response.data, null, 2));
-            });
-
-    }
-
-
-    return (
-        <div className={style["form-wrap"]}>
-            <form onSubmit={handleSubmit}>
-                <div className={style["input-wrap"]}>
-                    <div>id:</div>
-                    <input type="number" name="id" min="1" step="1" value={formData.id}
-                        onChange={handleChange} />
-                </div>
-                <div className={style["input-wrap"]}>
-                    <div>Year:</div>
-                    <input type="number" name="year" min="1" step="1" value={formData.year}
-                        onChange={handleChange} />
-                </div>
-                <div className={style["input-wrap"]}>
-                    <div>Value:</div>
-                    <input type="number" name="value" value={formData.value}
-                        onChange={handleChange} />
-                </div>
-                <div className={style["input-wrap"]}>
-                    <div>Measure Id:</div>
-                    <input type="number" name="measureId" min="1" step="1" max="2" value={formData.measureId}
-                        onChange={handleChange} />
-                </div>
-                <div className={style["input-wrap"]}>
-                    <div>Country Id:</div>
-                    <input type="number" name="countryId" min="1" step="1" max="14" value={formData.countryId}
-                        onChange={handleChange} />
-                    <button>Submit</button>
-                </div>
-            </form>
-
-
-            {gotData != 0 &&
-                <textarea rows={12}
-                    value={textAreaContent}
-                    onChange={e => setTextAreaContent(e.target.value)}
-                />
-            }
-        </div>)
+  return (
+    <div className={style["form-wrap"]}>
+      <p>DELETE</p>
+      <form onSubmit={handleSubmit}>
+        <button>Usuń</button>
+      </form>
+    </div>
+  );
 }
 
+function PutForm({ house, countryId }) {
+  const [formData, setFormData] = useState({
+    year: house.year,
+    value: house.value,
+    countryId: countryId,
+    measureId: "2",
+  });
+  const { token } = useAuth();
+
+  useEffect(() => {
+    setFormData({
+      year: house.year,
+      value: house.value,
+      countryId: countryId,
+      measureId: "2",
+    });
+  }, [house, countryId]);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let url = `housePrices/${house.id}`;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios
+      .put(url, formData, config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {});
+  }
+
+  return (
+    <div className={style["form-wrap"]}>
+      <p>PUT</p>
+      <form onSubmit={handleSubmit}>
+        <div className={style["input-wrap"]}>
+          <div>Rok:</div>
+          <input
+            type="number"
+            name="year"
+            min="1"
+            step="1"
+            value={formData.year}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={style["input-wrap"]}>
+          <div>Wartość:</div>
+          <input
+            type="number"
+            name="value"
+            value={formData.value}
+            onChange={handleChange}
+          />
+        </div>
+        <button>Wyślij</button>
+      </form>
+    </div>
+  );
+}
